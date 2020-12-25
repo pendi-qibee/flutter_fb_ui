f07338910b629fd3fdb35afbaee68b633956e54b
pendimadyana <pendimadyana@gmail.com>
2020-12-25 09:58:01 +0700

    add button icon live, photo, and room

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -12,7 +12,7 @@ class CreatePostContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
-      color: Colors.orange,
+      color: Colors.white,
       child: Column(
         children: [
           Row(
@@ -36,6 +36,40 @@ class CreatePostContainer extends StatelessWidget {
             height: 10.0,
             thickness: 0.5,
           ),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FlatButton.icon(
+                  onPressed: () => print('Live'),
+                  icon: const Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: Text('Live'),
+                ),
+                const VerticalDivider(width: 8.0),
+                FlatButton.icon(
+                  onPressed: () => print('Photo'),
+                  icon: const Icon(
+                    Icons.photo_library,
+                    color: Colors.green,
+                  ),
+                  label: Text('Photo'),
+                ),
+                const VerticalDivider(width: 8.0),
+                FlatButton.icon(
+                  onPressed: () => print('Room'),
+                  icon: const Icon(
+                    Icons.video_call,
+                    color: Colors.purpleAccent,
+                  ),
+                  label: Text('Room'),
+                ),
+              ], 
+            ),
+          )
         ],
       ),
     );
